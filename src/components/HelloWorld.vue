b12c997792669620048c83700aabab5923d7a5fd
Maksym <druzh.maks@gmail.com>
2018-08-30 19:18:28 +0300

    Test_commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,38 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+  <div>
+  <img :src="items[image].image" id="test">
+  <ul id="ul"> 
+  <li v-for="(item, key) in items">
+    <span @click="image = key"><img :src="item.image"/></span>
+  </li>
+</ul>
+</div>    
 </template>
-
+  
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
+import Router from '../router';
+export default{
+  name: 'app',
+  data: ()=> { 
+    let initImg = "bla1"
+    if (Router.currentRoute.query.image != undefined) {
+       initImg = Router.currentRoute.query.image 
+    }
     return {
-      msg: 'Welcome to Your Vue.js App'
+    image: initImg,
+    items: {
+     "bla1": { image: require('@/assets/Kar.jpg') },
+     "bla2": { image: require('@/assets/DK.jpg') },
+     "bla3": { image: require('@/assets/Lviv.jpg') },
+     "bla4": { image: require('@/assets/Shakhtar.jpg') },
+    }
+  }},
+  watch: {
+    // eslint-disable-next-line
+    image: value => {
+      Router.push({ query: { image: value }})
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
