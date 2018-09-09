d2283123c059cd75dfca075f5c729ceed4e548a7
Maksym <druzh.maks@gmail.com>
2018-09-09 19:24:38 +0300

    new_commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div>
   <img :src="items[image].image" id="test">
   <ul id="ul"> 
-  <li v-for="(item, key) in items">
+  <li v-for="(item, key) in items" id="li">
     <span @click="image = key"><img :src="item.image"/></span>
   </li>
 </ul>
@@ -13,9 +13,9 @@
 import Router from '../router';
 export default{
   name: 'app',
-  data: ()=> { 
+  data: function() { 
     let initImg = "bla1"
-    if (Router.currentRoute.query.image != undefined) {
+    if (this.$router.currentRoute.query.image != undefined) {
        initImg = Router.currentRoute.query.image 
     }
     return {
@@ -25,10 +25,13 @@ export default{
      "bla2": { image: require('@/assets/DK.jpg') },
      "bla3": { image: require('@/assets/Lviv.jpg') },
      "bla4": { image: require('@/assets/Shakhtar.jpg') },
+     "bla5": { image: require('@/assets/Vorskla.png') },
+     "bla6": { image: require('@/assets/Mariupol.gif') },
+     "bla7": { image: require('@/assets/Zorya.png') },
+     "bla8": { image: require('@/assets/Olexa.jpg'), },
     }
   }},
   watch: {
-    // eslint-disable-next-line
     image: value => {
       Router.push({ query: { image: value }})
     }
@@ -40,10 +43,15 @@ export default{
 #test {
     width: 500px;
     height: 500px;
+    opacity: 0.2;
 }
 #ul{
   list-style-type: none;
-  display: inline-block;
+  /* display: inline-block; */
+}
+#li {
+  display: inline;
+  margin-left: 10px;
 }
 </style>
 
